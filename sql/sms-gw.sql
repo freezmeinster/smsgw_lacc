dc201f500b2a8a056596cad88b83a3f3660363dc
freezmeinster <freez_meinster@yahoo.co.id>
2011-05-21 02:22:43 -0800

    lupa nambah sql nya

--- sql/sms-gw.sql
+++ sql/sms-gw.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 18, 2011 at 09:01 PM
+-- Generation Time: May 21, 2011 at 02:22 AM
 -- Server version: 5.1.56
 -- PHP Version: 5.3.6
 
@@ -135,8 +135,11 @@ CREATE TABLE IF NOT EXISTS `sms_group` (
 
 CREATE TABLE IF NOT EXISTS `sms_inbox` (
   `id_sms` int(10) NOT NULL AUTO_INCREMENT,
+  `status_baca` tinyint(1) NOT NULL DEFAULT '0',
+  `no_kontak` varchar(20) NOT NULL,
   `isi_sms` text NOT NULL,
-  `id_kriteria` int(11) NOT NULL,
+  `id_kriteria` int(11) DEFAULT NULL,
+  `waktu_masuk` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_sms`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -158,27 +161,45 @@ CREATE TABLE IF NOT EXISTS `sms_outbox` (
   `isi_sms` text NOT NULL,
   `id_kriteria` int(11) NOT NULL,
   `waktu_masuk` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status_kirim` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_sms`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=34 ;
 
 --
 -- Dumping data for table `sms_outbox`
 --
 
-INSERT INTO `sms_outbox` (`id_sms`, `id_kontak`, `no_kontak`, `isi_sms`, `id_kriteria`, `waktu_masuk`) VALUES
-(5, 1, NULL, 'Bangun', 1, '2011-05-17 23:41:31'),
-(6, 7, NULL, '', 1, '2011-05-18 17:56:50'),
-(7, 7, NULL, 'gffffjjjjjhgfjhgfjgh', 1, '2011-05-18 17:57:10'),
-(3, 7, NULL, 'Hai aihai', 5, '2011-05-17 18:52:53'),
-(4, 1, NULL, 'guck fucvkasdfasdfasdfasdfasdfasdfasdfasd', 1, '2011-05-17 18:53:50'),
-(8, 1, NULL, 'sdfgsdfgsdfg', 2, '2011-05-18 17:57:28'),
-(9, 1, NULL, 'hallo<br>', 2, '2011-05-18 17:59:27'),
-(10, 7, NULL, 'Fuck diki', 5, '2011-05-18 18:03:26'),
-(11, 1, NULL, '&nbsp;nguk nguk nguk', 1, '2011-05-18 18:03:55'),
-(12, 1, '085279916229', 'ajsdfnajklsdnflkjasdnfkj', 5, '2011-05-18 18:23:51'),
-(13, 0, '085721176190', 'sdfasd', 5, '2011-05-18 18:25:21'),
-(14, 1, '085279916229', 'dftydtydrtyrty', 2, '2011-05-18 18:56:57'),
-(15, 8, '08997773073', 'Maaf', 1, '2011-05-18 20:57:23');
+INSERT INTO `sms_outbox` (`id_sms`, `id_kontak`, `no_kontak`, `isi_sms`, `id_kriteria`, `waktu_masuk`, `status_kirim`) VALUES
+(5, 1, NULL, 'Bangunf', 1, '2011-05-17 23:41:31', 1),
+(6, 7, NULL, 'jhjh', 1, '2011-05-18 17:56:50', 1),
+(7, 7, NULL, 'gffffjjjjjhgfjhgfjgh', 1, '2011-05-18 17:57:10', 1),
+(3, 7, NULL, 'Hai aihai', 5, '2011-05-17 18:52:53', 1),
+(4, 1, NULL, 'guck fucvkasdfasdfasdfasdfasdfasdfasdfasd', 1, '2011-05-17 18:53:50', 1),
+(8, 1, NULL, 'sdfgsdfgsdfg', 2, '2011-05-18 17:57:28', 1),
+(9, 1, NULL, 'hallo<br>', 2, '2011-05-18 17:59:27', 1),
+(10, 7, NULL, 'Fuck diki', 5, '2011-05-18 18:03:26', 1),
+(11, 1, NULL, '&nbsp;nguk nguk nguk', 1, '2011-05-18 18:03:55', 1),
+(12, 1, '085279916229', 'ajsdfnajklsdnflkjasdnfkj', 5, '2011-05-18 18:23:51', 1),
+(13, 0, '085721176190', 'sdfasd', 5, '2011-05-18 18:25:21', 1),
+(14, 1, '085279916229', 'dftydtydrtyrty', 2, '2011-05-18 18:56:57', 1),
+(15, 8, '08997773073', 'Maaf', 1, '2011-05-18 20:57:23', 1),
+(16, 1, '085279916229', 'Fuck ', 5, '2011-05-20 23:19:40', 1),
+(17, 3, '089662048911', 'asfasfasdfa', 2, '2011-05-20 23:21:20', 1),
+(18, 3, '089662048911', 'uksdflksdkfsklfmskdmfksdmfsk;dmfklsdmfklsmdflksmdlkfmsdfks<br>sl;kdmfk;lsdmfk;lsdmflk;smdfs<br>df;smdf;klms;dklfms;dkmf;lksdmfk;lsdmfklsmdf<br>sdfksmdfkl;msdk;l', 1, '2011-05-21 00:38:37', 1),
+(19, 3, '089662048911', 'fmsd;lkfms;ldmf;lskdmf;lksdmfk;lsmdfs<br>dfl;sdmfk;lsmd;fkmsd;klmfs;lkdmf;slkdmf;lksmdf;skldf<br>sdkflmsdk;lfmsk;ldmf;skldmf;lksdmf;slkdmfsd<br>', 1, '2011-05-21 00:38:37', 1),
+(20, 3, '089662048911', 'opopopopopo<br>', 2, '2011-05-21 00:40:37', 1),
+(21, 3, '089662048911', 'asdfasd', 2, '2011-05-21 00:42:02', 1),
+(22, 3, '089662048911', 'asdfasdasdfasdf', 1, '2011-05-21 00:50:58', 1),
+(23, 3, '089662048911', 'asd34tw er', 5, '2011-05-21 01:00:46', 1),
+(24, 3, '089662048911', 'slkmfklamsfasdf', 1, '2011-05-21 01:18:30', 1),
+(25, 3, '089662048911', 'kamsdkfmaksldfa', 1, '2011-05-21 01:18:37', 1),
+(26, 3, '089662048911', 'asli lah , lapar', 2, '2011-05-21 02:08:44', 1),
+(27, 3, '089662048911', 'asdfasdf asdfkmasdf asdfasdfas', 5, '2011-05-21 02:10:56', 1),
+(29, 3, '089662048911', 'asdfasd', 6, '2011-05-21 02:12:29', 1),
+(30, 1, '085279916229', 'asdfasdfasdfasdna sdfasdf a sdf as f asd fa sdf a sdf a dsf a sdf as df ads f a', 1, '2011-05-21 02:14:07', 1),
+(31, 1, '085279916229', 'askdfkasdnlfjanlsdfn;lasdnflajsdnf;lajdnf<br>asdfnasdjfnalkjsdnfkjalsdnfkjansdklfjnsdfadsfjasdnfjlasdnfjlkansdjkfnads<br>fajsdnfljandfjandsjklfansdjfnas;djfands', 1, '2011-05-21 02:14:55', 1),
+(32, 1, '085279916229', '<br>fajsdnfljasdnfl;jandfl;jansdfl;jnasdlfkjnasd;kfnasd<br>fajlsdnflja;sdnf;ljadnsf;jlaf;ansdlfjandslj;fnasd;lfnasd<br>falkdsnfl;asndflkansdf;klansd;lfkansd;lkf', 1, '2011-05-21 02:14:55', 1),
+(33, 1, '085279916229', 'ads', 1, '2011-05-21 02:14:55', 1);
 
 -- --------------------------------------------------------
 
